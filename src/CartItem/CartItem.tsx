f1ad6558e90fa86c2b90f472c4b877c06d880ed4
JakeBaek-SFX <jake.baek@soulfx.com>
2021-07-20 16:27:58 -0400

    first commit

---
+++ src/CartItem/CartItem.tsx
@@ -0,0 +1,9 @@
+import Button from '@material-ui/core/Button';
+//type
+import { CartItemType } from '../App';
+//styles
+import { Wrapper } from "./CartItem.styles";
+
+const CartItem: React.FC = () => <div>CartItem</div>
+
+export default CartItem;
